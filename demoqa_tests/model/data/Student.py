8be0d0a067395acb50525f9ed6e91050ae704af7
Alexander Santalov <vgtrk520@yandex.ru>
2022-12-30 17:16:46 +0300

    fix_class_student

--- demoqa_tests/model/data/Student.py
+++ demoqa_tests/model/data/Student.py
@@ -5,24 +5,6 @@ from enum import Enum
 from typing import Literal, List
 
 
-class Student:
-
-    def __init__(self, first_name, last_name, email, phone, address, birthday, gender, subject, hobby, image,
-                 state, city):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.email = email
-        self.phone = phone
-        self.address = address
-        self.birthday = birthday
-        self.gender = gender
-        self.subject = subject
-        self.hobby = hobby
-        self.image = image
-        self.state = state
-        self.city = city
-
-
 class Hobby(Enum):
     Music = 1,
     Reading = 2,
@@ -30,7 +12,7 @@ class Hobby(Enum):
 
 
 @dataclass
-class Student_:
+class Student:
     first_name: str
     last_name: str
     email: str
@@ -46,7 +28,7 @@ class Student_:
     city: Literal['Karnal', 'Panipat', 'Delhi', 'Gurgaon', 'Noida', 'Agra', 'Merrut', 'Lucknow', 'Jaipur', 'Jaiselmer']
 
 
-a_santalov = Student_(
+a_santalov = Student(
     first_name='Alexander',
     last_name='Santalov',
     email='asantalov@bolid.ru',
